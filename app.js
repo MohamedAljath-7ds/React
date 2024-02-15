0e197a1908bdc453d42397ba17dee5a8ff9f2e20
Aljath <mohamedaljathn@gmail.com>
2024-02-15 14:42:25 +0530

    search

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const Card = ({filters}) => {
             <div className="cards">
               <img className="logo" src={filters.img}/>
               <img className="star" src="https://th.bing.com/th/id/OIP.mwnZJFazxV47evELY1FphgHaFw?rs=1&pid=ImgDetMain"/>
+              <h2 className="name-center">{filters.name}</h2>
               <h3>{filters.Rating}</h3>
             </div>
        
@@ -30,26 +31,32 @@ const Card = ({filters}) => {
 const Body = () => {
     const[filterData, setfilterData] = useState(
         [{
+            name:"pizza",
             img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/terdirvzcbmbwok1bwsu",
             Rating:"4.5",
         },
         {
+            name:"noodels",
             img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0839ff574213e6f35b3899ebf1fc597",
             Rating:"4.5",
         },
-        { 
+        {  
+           name:"burger", 
            img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aa77cb6fce4d94f3ad4b4e7fb42782db", 
            Rating:"2.5",
         },
         { 
+            name:"maxco",
             img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aa77cb6fce4d94f3ad4b4e7fb42782db", 
             Rating:"2.2",
          },
-         { 
+         {  
+            name:"biriyani",
             img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aa77cb6fce4d94f3ad4b4e7fb42782db", 
             Rating:"2.4",
          },
          { 
+            name:"rasam",
             img:"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/aa77cb6fce4d94f3ad4b4e7fb42782db", 
             Rating:"2.6",
          },
@@ -57,19 +64,31 @@ const Body = () => {
         ]
         
     );
+    const[searchs, setsearchs] = useState("");
     return (
-        <>
+        <div className="container">
         <button className="btn" onClick={() => {
             let a = filterData.filter((single) => single.Rating > 3);
             setfilterData(a);
         } }>Top Rating
         </button>
+        <div className="input-search">
+        <input type="text" placeholder="search" value={searchs} onChange={
+            (e) => {
+                setsearchs(e.target.value);
+            }
+        }></input>
+        <button onClick={() => {
+            const fD = filterData.filter((single) => single.name.toLowerCase().includes(searchs.toLowerCase()));
+            setfilterData(fD);
+        }}>search</button>
+        </div>
         <div className="body-wrapper">
                 
                 {filterData.map((single) => <Card filters={single} />)}
 
         </div>
-        </>
+        </div>
     )
 }
 
